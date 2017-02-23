6b2f2cf8af5b5b9695d8b5c32bfc868e8215fc56
Mathieu Ripert <mathieu.ripert@gmail.com>
2017-02-23 11:11:48 -0800

    version 0.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ required = [
 
 setup(
     name="instadb",
-    version="0.1",
+    version="0.2",
     author="Mathieu Ripert",
     author_email="mathieu@instacart.com",
     url="https://github.com/mathieuripert/instadb",
