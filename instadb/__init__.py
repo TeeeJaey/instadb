e6c8d191b5a22c50e4f9b23daacb870567dd8c58
Mathieu Ripert <mathieu.ripert@gmail.com>
2017-10-06 10:09:28 -0700

    list connections available from env

--- instadb/__init__.py
+++ instadb/__init__.py
@@ -9,7 +9,9 @@ from instadb.connection import Connection
 logger = logging.getLogger(__name__)
 
 connect = False
+vars()["connections"] = []
 for key in os.environ:
   match = re.match('(.*)_DATABASE_URL', key)
   if match:
     vars()[match.group(1)] = Connection(os.environ.get(match.group()), connect=connect)
+    vars()["connections"].append(match.group(1))
