b2157b634f15f11b4b65eb6c6e6e2cb59e45ec6b
Mathieu Ripert <mathieu.ripert@gmail.com>
2017-10-05 23:19:25 -0700

    refactor + rename + 0.3 version

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 from setuptools import find_packages
 
 required = [
-    "pandas>=0.15.2",
-    "psycopg2==2.6.2"
+  "psycopg2"
+  "pandas",
 ]
 
 setup(
     name="instadb",
-    version="0.2",
+    version="0.3",
     author="Mathieu Ripert",
     author_email="mathieu@instacart.com",
     url="https://github.com/mathieuripert/instadb",
@@ -17,19 +17,7 @@ setup(
     packages=find_packages(),
     package_dir={"instadb": "instadb"},
     description="A simple and light DB package",
+    long_description=open("README.rst").read(),
     install_requires=required,
-    classifiers=[
-        # Maturity
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-        # License
-        'License :: OSI Approved :: BSD License',
-        # Versions supported
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-    ],
 
 )
